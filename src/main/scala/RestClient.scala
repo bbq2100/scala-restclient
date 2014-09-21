a112c9161f889b31779236c8406e46ad1d52c952
Qaiser Abbasi <q.abbasi1010@gmail.com>
2014-09-21 09:53:50 +0200

    implemented POST operation

--- src/main/scala/RestClient.scala
+++ src/main/scala/RestClient.scala
@@ -1,7 +1,15 @@
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.{BasicResponseHandler, DefaultHttpClient}
-import org.apache.http.message.BasicHeader
+import java.util
 
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.{HttpPost, HttpGet}
+import org.apache.http.impl.client.{BasicResponseHandler, DefaultHttpClient}
+import org.apache.http.message.{BasicNameValuePair, BasicHeader}
+
+/**
+ * Provides a convenient way to call RESTful webservices.
+ * How to run: sbt run (post | get | delete | options) -d <request parameters comma separated -h <headers comma separated> <url>
+ *(at least you should specify action(post, get, delete, options) and server url)
+ */
 object RestClient extends App {
   require(args.size >= 2, "At minimum you should specify the HTTP operation (Get, Post, Put, Delete, Options) and the server url")
 
@@ -10,7 +18,9 @@ object RestClient extends App {
   val url = args.last
 
   def parseArgs(args: Array[String]): Map[String, List[String]] = {
+
     def nameValuePair(paramName: String) = {
+
       def values(commaSeparatedValues: String) = {
         commaSeparatedValues.split(",").toList
       }
@@ -30,12 +40,12 @@ object RestClient extends App {
     case "options" => handleOptionRequest
   }
 
-  def handleGetRequest = {
-    def headers = for (header <- params("-h")) yield {
-      def tokens = splitByEqual(header)
-      new BasicHeader(tokens(0), tokens(1))
-    }
+  def headers = for (header <- params(key = "-h")) yield {
+    def tokens = splitByEqual(header)
+    new BasicHeader(tokens(0), tokens(1))
+  }
 
+  def handleGetRequest = {
     val query = params("-d").mkString("&")
     /**
      * Applying string interpolation (injecting local variables in the result String object) via StringContext.
@@ -51,7 +61,33 @@ object RestClient extends App {
 
   def handleOptionRequest = ???
 
-  def handlePostRequest = ???
+  def handlePostRequest = {
+
+    def formEntity = {
+      def toJavaList(scalaList: List[BasicNameValuePair]) = {
+        /**
+         * Enforcing to send the result of toArray() as variable argument to the asList().
+         * Otherwise asList() will create only one item.
+         * @see List.toArray:_*
+         */
+        util.Arrays.asList(scalaList.toArray:_*)
+      }
+
+      def formParams = for(nameValue <- params("-d")) yield {
+        def tokens = splitByEqual(nameValue)
+        new BasicNameValuePair(tokens(0), tokens(1))
+      }
+
+      def formEntity = new UrlEncodedFormEntity(toJavaList(formParams), "UTF-8")
+      formEntity
+    }
+
+    val httpPost: HttpPost = new HttpPost(url)
+    headers.foreach(httpPost.addHeader(_))
+    httpPost.setEntity(formEntity)
+    val responseBody = new DefaultHttpClient().execute(httpPost, new BasicResponseHandler)
+    println(responseBody)
+  }
 
   def handlePutRequest = ???
 
